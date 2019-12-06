b29673ac49f66ac86a4adb7c62ac5d1164cb9f11
Wetoria <vwetoria@gmail.com>
2019-12-06 10:43:32 +0800

    Test flutter & unit-app

--- Demos/Flutter/my_app/lib/main.dart
+++ Demos/Flutter/my_app/lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:english_words/english_words.dart';
-import 'package:flutter/semantics.dart';
 
 void main() => runApp(new MyApp());
 
@@ -9,7 +11,70 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Welcome to Flutter',
-      home: new RandomWords(),
+      home: new FlutterDemo(),
+    );
+  }
+}
+
+class FlutterDemo extends StatefulWidget {
+  FlutterDemo({Key key}) : super(key: key);
+
+  @override
+  _FlutterDemoState createState() => new _FlutterDemoState();
+}
+
+class _FlutterDemoState extends State<FlutterDemo> {
+  int _counter;
+
+  @override
+  void initState() {
+    super.initState();
+    _readCounter().then((int value) {
+      setState(() {
+        _counter = value;
+      });
+    });
+  }
+
+  Future<File> _getLocalFile() async {
+    // get the path to the document directory.
+    String dir = (await getExternalStorageDirectory()).path;
+    return new File('$dir/counter.txt');
+  }
+
+  Future<int> _readCounter() async {
+    try {
+      File file = await _getLocalFile();
+      // read the variable as a string from the file.
+      String contents = await file.readAsString();
+      return int.parse(contents);
+    } on FileSystemException {
+      return 0;
+    }
+  }
+
+  Future<Null> _incrementCounter() async {
+    setState(() {
+      _counter++;
+    });
+    // write the variable as a string to the file
+    await (await _getLocalFile()).writeAsString('{ test: "test",\n t: "T" }');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text('Flutter Demo')),
+      body: new Center(
+        child: new Text('Button tapped $_counter time${
+          _counter == 1 ? '' : 's'
+        }.'),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ),
     );
   }
 }
