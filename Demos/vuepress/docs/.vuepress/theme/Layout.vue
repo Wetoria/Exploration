f28727c2c10e52acd23cc41a1cc80535fc5cf954
Wetoria <vwetoria@gmail.com>
2020-01-13 14:41:46 +0800

    Test VuePress

---
+++ Demos/vuepress/docs/.vuepress/theme/Layout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="main">
+    <div class="user-info-container"></div>
+    <img src="../public/images/bg4.jpg" />
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log(this.$router, this.$route, this.$page, this.$site);
+  }
+}
+</script>
---
+++ Demos/vuepress/docs/README.md
@@ -0,0 +1,15 @@
+# Hello VuePress!
+
+## Test <sup>[t](#1)</sup>
+
+This is a text sentence
+
+```js
+export default {
+  data: data
+}
+```
+
+<p v-for="i in 3">{{ i }}</p>
+
+<span id="1"></span>
