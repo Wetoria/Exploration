8cc19d151a20fe0b756cfba1564d9e6b2f4aa369
Wetoria <vwetoria@gmail.com>
2020-01-16 16:30:35 +0800

    Update theme

--- Demos/vuepress/docs/.vuepress/theme/Layout.vue
+++ Demos/vuepress/docs/.vuepress/theme/Layout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="main">
+  <div class="container">
     <div class="user-info-main" v-if="isHome || isPostDetail">
       <div class="info-container">
         <a href="/" title="前往 Wetoria 的主页">
@@ -18,7 +18,7 @@
             </ul>
           </div>
           <div class="outside">
-            <a :href="$site.themeConfig.github" target="_blank">
+            <a @click="jumpToGitHub">
               <v-icon icon-class="v-github" />
             </a>
           </div>
@@ -26,28 +26,34 @@
       </div>
       <div class="cover"></div>
     </div>
-    <div class="post-container">
-      <div class="post-list" v-if="isHome">
-        <div class="post-item" v-for="(page, index) in posts" :key="page.key">
-          <h2 class="post-title">
-            <a :href="page.regularPath">{{ page.title }}</a>
-          </h2>
-          <p class="post-excerpt" v-if="page.excerpt" v-html="page.excerpt"></p>
-          <div class="post-meta">
-            <time>{{ getDate(page.lastUpdated) }}</time>
-            <span class="tags" v-if="shouldRenderTags(page)">
-             • 于 
-              <span class="tag" v-for="tag in page.frontmatter.tags">
-                <a class="tag-link">{{tag}}</a>
+    <div class="main">
+      <div class="post-container">
+        <div class="post-list" v-if="isHome">
+          <div class="post-item" v-for="(page, index) in posts" :key="page.key">
+            <h2 class="post-title">
+              <a :href="page.regularPath">{{ page.title }}</a>
+            </h2>
+            <p class="post-excerpt" v-if="page.excerpt" v-html="page.excerpt"></p>
+            <div class="post-meta">
+              <time>{{ getDate(page.lastUpdated) }}</time>
+              <span class="tags" v-if="shouldRenderTags(page)">
+              • 于 
+                <span class="tag" v-for="tag in page.frontmatter.tags">
+                  <a class="tag-link">{{tag}}</a>
+                </span>
               </span>
-            </span>
-            <a :href="page.regularPath" class="btn-continue">继续阅读</a>
+              <a :href="page.regularPath" class="btn-continue">继续阅读</a>
+            </div>
+            <hr class="post-divider" v-if="index !== posts.length - 1" />
           </div>
-          <hr class="post-divider" v-if="index !== posts.length - 1" />
+        </div>
+        <div class="post-detail" v-if="isPostDetail">
+          <h1><a :href="$page.regularPath">{{ $page.title }}</a></h1>
+          <Content />
         </div>
       </div>
-      <div class="post-detail" v-if="isPostDetail">
-        <Content />
+      <div class="footer">
+        <span>&copy; 2020 - Power by <a @click="jumpToGitHub">Wetoria</a></span>
       </div>
     </div>
   </div>
@@ -82,6 +88,9 @@ export default {
     },
     shouldRenderTags(page) {
       return page.frontmatter.tags && page.frontmatter.tags.length;
+    },
+    jumpToGitHub() {
+      window.open(this.$site.themeConfig.github, '_blank');
     },
   }
 }
